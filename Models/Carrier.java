f8b9a6058af0edcff7fab01adaaa5e82ae7a14f4
max232st <max232st@gmail.com>
2023-08-04 11:01:13 +0300

    dz4

---
+++ Models/Carrier.java
@@ -0,0 +1,23 @@
+package Models;
+
+/**
+ * Модель перевозчика
+ */
+public class Carrier {
+    private int id;
+    private long cardNumber;
+
+    public long getCardNumber() {
+        return cardNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Carrier(int id, long cardNumber) {
+        this.id = id;
+        this.cardNumber = cardNumber;
+    }
+
+}
